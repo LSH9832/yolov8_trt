482617ba808f1439a891554a1bc4f980bfa1f16b
Shihan Liu <69880398+LSH9832@users.noreply.github.com>
2023-01-13 20:22:47 +0800

    Add files via upload

---
+++ ultralytics/yolo/configs/__init__.py
@@ -0,0 +1,38 @@
+# Ultralytics YOLO 🚀, GPL-3.0 license
+
+from pathlib import Path
+from typing import Dict, Union
+
+from omegaconf import DictConfig, OmegaConf
+
+from ultralytics.yolo.configs.hydra_patch import check_config_mismatch
+
+
+def get_config(config: Union[str, DictConfig], overrides: Union[str, Dict] = None, strict=True):
+    """
+    Load and merge configuration data from a file or dictionary.
+
+    Args:
+        config (Union[str, DictConfig]): Configuration data in the form of a file name or a DictConfig object.
+        overrides (Union[str, Dict], optional): Overrides in the form of a file name or a dictionary. Default is None.
+
+    Returns:
+        OmegaConf.Namespace: Training arguments namespace.
+    """
+    if overrides is None:
+        overrides = {}
+    if isinstance(config, (str, Path)):
+        config = OmegaConf.load(config)
+    elif isinstance(config, Dict):
+        config = OmegaConf.create(config)
+    # override
+    if isinstance(overrides, str):
+        overrides = OmegaConf.load(overrides)
+    elif isinstance(overrides, Dict):
+        overrides = OmegaConf.create(overrides)
+
+    for key in check_config_mismatch(dict(overrides).keys(), dict(config).keys(), strict=strict):
+        if not strict:
+            config.__setattr__(key, overrides[key])
+
+    return OmegaConf.merge(config, overrides)
